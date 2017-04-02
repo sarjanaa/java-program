dff62e966aecaecada354894bd4ffc4331013b09
sarjanaa <ssarjanaa@gmail.com>
2017-04-02 14:28:20 +0530

    second

---
+++ Arithmetic.java
@@ -0,0 +1,17 @@
+package second;
+
+public class Arithmetic {
+ public static void main(String[] args) {
+    int a=2,b=5,c;
+    c=a+b;
+    int d=a-b;
+    int e=a*b;
+    int f=a/b;
+    System.out.println("The addition is:"+c);
+    System.out.println("The subtraction is:"+d);
+    System.out.println("The multiplication is:"+e);
+    System.out.println("The division is:"+f);
+}
+	 
+ }
+
