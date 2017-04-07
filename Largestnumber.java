742e0e6b22a9756bd4af78daed377bfafa166e0b
sarjanaa <ssarjanaa@gmail.com>
2017-04-07 15:46:24 +0530

    This is my third commit

---
+++ Largestnumber.java
@@ -0,0 +1,20 @@
+package largestnumber;
+
+public class Largestnumber {
+public static void main(String ags[])
+{
+	int a=5,b=8,c=2;
+	if(a>b && a>c)
+	{
+		System.out.println("The largest number is:"+a);
+	}
+	else if(b>a && b>c)
+	{
+		System.out.println("The largest number is:"+b);
+	}
+	else
+	{
+		System.out.println("The largest number is:"+c);
+	}
+}
+}
