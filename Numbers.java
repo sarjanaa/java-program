15d292f6ed771f4e98ddbef5ac9dda91a2ef8d33
sarjanaa <ssarjanaa@gmail.com>
2017-04-09 12:14:16 +0530

    This is my fourth commit

---
+++ Numbers.java
@@ -0,0 +1,12 @@
+
+public class Numbers {
+     static int a=20;
+     public static void main(String[] args) {
+		int b=10;
+		System.out.println("b:"+b);
+		System.out.println(b);
+		a=30;
+		System.out.println(Numbers.a);
+		
+	}
+}
