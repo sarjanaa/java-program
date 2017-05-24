8e5073daef5a353b77b91a170d30e6ddff5a6234
sarjanaa <ssarjanaa@gmail.com>
2017-05-24 11:42:10 +0530

    nine

---
+++ Stringoperations.java
@@ -0,0 +1,17 @@
+package stringoperations;
+
+public class Stringoperations {
+         public static void main(String[] args) {
+			String s1="welcome";
+			String s2=s1.substring(2,5);
+			System.out.println(s1.charAt(3));
+			System.out.println(s2);
+			System.out.println(s1.indexOf('w'));
+			System.out.println(s1.lastIndexOf('e'));
+			System.out.println(s1.length());
+			s1.trim();
+			System.out.println(s1.length());
+			
+					
+		}
+}
