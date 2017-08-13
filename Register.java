dfc4e1087aaf83d11fdf853a8c972cab476e0116
sarjanaa <ssarjanaa@gmail.com>
2017-08-13 14:48:55 +0530

    14

--- Register.java
+++ Register.java
@@ -1,151 +1,30 @@
-package registration;
-import java.util.*;
+package web;
 
-public class Register {
-	String name; 
-	String age;
-	String usr;
-	String pwd;
-	private Scanner sc;
-	private Scanner sc2;
-	private Scanner sc3;
-	private Scanner sc4;
-	
-	public boolean validatename(String n)
-	{
-		String regex = "^[a-zA-Z]+$";
-		n=n.trim();
-		if (n.matches(regex))
-		{
-			return true;
-		}
-		else
-		{
-			System.out.println("Only characters allowed. Please re-enter the name");
-			return false;
-		}
-		
-	}
-	public boolean validateage(String n)
-	{
-		String regex = "^[0-9]+$";
-		age=age.trim();
-		if (age.matches(regex))
-		{
-			return true;
-		}
-		else
-		{
-			System.out.println("Only numbers allowed. Please re-enter the age");
-			return false;
-		}
-	}
-	public boolean validateusername(String usr)
-	{
-		String regex = "^[a-zA-Z0-9]+$";
-		usr=usr.trim();
-		if (usr.length() > 6) 
-		{
-		System.out.println("Only 6 cha allowed. Re-enter the username");
-		return false;
-		}
-		else 
-			if (usr.matches(regex))
-				{
-				return true;
-				}
-				else
-				{
-				System.out.println("No Special cha allowed. Re-enter the username");
-				return false;	
-				}
-	 }
-	public boolean validatepwd(String pwd)
-	{
-		String regex = "^[a-zA-Z0-9@!_$*#]+$";
-		pwd=pwd.trim();
-		if (pwd.length() > 8)
-		{
-			System.out.println("Only 8 cha allowed. Re-enter the pwd");
-			return false;
-			}
-			else 
-				if (pwd.matches(regex))
-					{
-					return true;
-					}
-					else
-					{
-					System.out.println("Entered invalid cha. Re-enter the pwd");
-					return false;	
-					}
-	}
-	public boolean readName()
-	{
-		sc = new Scanner(System.in);
-		System.out.println("Enter your name");
-		name = sc.next();
-		boolean a=validatename(name);
-		return a;
-	}
-	public boolean getage()
-	{
-		sc2 = new Scanner(System.in);
-		System.out.println("Enter your age");
-		age = sc2.next();
-		boolean b=validateage(age);
-		return b;
-	}
-	public boolean getusr()
-	{
-		sc3 = new Scanner(System.in);
-		System.out.println("Enter your Username");
-		usr = sc3.next();
-		boolean c=validateusername(usr);
-		return c;
-	}
-	
-	public boolean getpwd()
-	{
-		sc4 = new Scanner(System.in);
-		System.out.println("Enter your Password");
-		pwd = sc4.next();
-		boolean d=validatepwd(pwd);
-		return d;
-	}
-     public static void main(String[] args) {
-    	 Register r1 = new Register ();
- 		if(!r1.readName())
- 		{
- 			r1.readName();
- 		}
- 		System.out.println("Name : "+r1.name);
- 		if (!r1.getage())
-		{
-			System.out.println("false boolean age");
-			r1.getage();
-		}
-		System.out.println("Age : "+r1.age);
-		
-		if (!r1.getusr())
-		{
-			System.out.println("false boolean username");
-			r1.getusr();
-		}
-		System.out.println("Username: "+r1.usr);
-		
-		if (!r1.getpwd())
-		{
-			r1.getpwd();
-		}
-		System.out.println("Password: "+r1.pwd);
-		System.out.println("Your Registration is sucess!");
-		//System.out.println("Login form");
-		//System.out.println("----------");		
-		//System.out.println("Enter the username");
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * Servlet implementation class Register
+ */
+@WebServlet("/loginServlet")
+public class Register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
-     }
-	
-	
+    	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter pw=response.getWriter();
+		response.setContentType("text/html");
+		String username=request.getParameter("u");
+		String password=request.getParameter("p");
+		if(username.equals("sarjanaa")&&password.equals("sarjanaa"))
+			pw.println("Login successful");
+		else
+			pw.println("Login failed");
+	}
 
 }
